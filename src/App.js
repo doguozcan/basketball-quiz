728b3de503ef64d8fca29af8900312724717c1f7
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-02-15 10:30:17 +0300

    Responsive design enhancements

--- src/App.js
+++ src/App.js
@@ -27,14 +27,14 @@ export default function App() {
   }
 
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center bg-[#2B2E4A] font-protest_strike text-white">
+    <div className="h-screen w-full flex flex-col justify-center items-center font-protest_strike text-white">
       {showScore ? (
         score >= questions.length / 2 ? (
           <p className="text-3xl text-center fadeIn">
             Congrats you got {score} out of {questions.length} 🎉
           </p>
         ) : (
-          <p className="text-3xl text-center fadeIn">
+          <p className="text-lg sm:text-3xl text-center fadeIn">
             You got {score} out of {questions.length}. You can practice your
             basketball knowledge via{' '}
             <a
@@ -49,21 +49,21 @@ export default function App() {
         )
       ) : (
         <>
-          <span className="text-3xl mb-3">
+          <span className="text-xl md:text-3xl mb-3">
             Question {currentQuestion + 1}/{questions.length}
           </span>
-          <div className="flex justify-between p-2 gap-2 w-4/6 h-4/6 bg-[#E84545] border border-[#903749] rounded-md shadow-lg">
-            <div className="w-1/2 p-4 flex flex-col justify-center">
-              <p className="flex items-center text-lg font-medium shadow-lg p-2">
+          <div className="flex flex-col sm:flex-row justify-between p-2 gap-2 bg-[#E84545] border border-[#903749] rounded-md shadow-lg">
+            <div className="p-4 flex flex-col justify-center">
+              <p className="flex items-center text-sm md:text-lg font-medium shadow-lg p-2">
                 {questions[currentQuestion].questionText}
               </p>
               <p className="text-center mt-5 min-h-6">{feedback}</p>
             </div>
-            <div className="w-1/2 flex flex-col justify-between p-2 shadow-lg">
+            <div className="flex flex-col justify-between p-2 gap-2 shadow-lg">
               {questions[currentQuestion].questionOptions.map((option) => (
                 <button
                   disabled={feedback.length !== 0}
-                  className={`border p-2 rounded-md shadow-lg text-md ${
+                  className={`border p-2 rounded-md shadow-lg text-sm md:text-md ${
                     feedback.length !== 0
                       ? option.isCorrect
                         ? 'bg-green-500 border-3'
